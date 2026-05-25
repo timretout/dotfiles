a1593f915244ef7ae9727e967de85b118a8deb10
Tim Retout <tim@retout.co.uk>
2026-05-25 15:40:44 +0100

    Disable tabs in Firefox

---
+++ dot_mozilla/private_firefox/personal.default/user.js
@@ -0,0 +1,8 @@
+// Enable custom CSS stylesheets (userChrome.css)
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Prevent middle-clicks from opening hidden background tabs
+user_pref("browser.tabs.opentabfor.middleclick", false);
+
+// Force middle-clicks to open fresh OS windows for Sway to tile
+user_pref("middlemouse.openNewWindow", true);
